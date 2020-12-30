962de84bbbe7a733ec9f970ba9ed6ab3d5f60437
Andrea Di Giorgi <adigiorgi@outlook.com>
2020-12-29 18:43:44 -0800

    Complete topic UI

--- discuss/lib/discuss/topic.ex
+++ discuss/lib/discuss/topic.ex
@@ -1,4 +1,4 @@
-defmodule Discuss.Topics.Topic do
+defmodule Discuss.Topic do
   use Ecto.Schema
   import Ecto.Changeset
 
@@ -6,7 +6,7 @@ defmodule Discuss.Topics.Topic do
     field :title, :string
   end
 
-  def changeset(topic, attrs) do
+  def changeset(topic, attrs \\ %{}) do
     topic
     |> cast(attrs, [:title])
     |> validate_required([:title])
