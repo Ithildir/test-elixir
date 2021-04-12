4e4fb21bfcd61107a3aa005460865e9a7c94a3d9
Andrea Di Giorgi <adigiorgi@outlook.com>
2021-04-12 08:25:52 -0700

    Start of comments

---
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -0,0 +1,32 @@
+defmodule DiscussWeb.CommentsChannel do
+  use DiscussWeb, :channel
+  alias Discuss.{Comment, Repo, Topic}
+
+  def handle_in(_name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset =
+      topic
+      |> Ecto.build_assoc(:comments)
+      |> Comment.changeset(%{content: content})
+
+    case Repo.insert(changeset) do
+      {:ok, _comment} ->
+        {:reply, :ok, socket}
+
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+  end
+
+  def join("comments:" <> topic_id, _params, socket) do
+    topic_id = String.to_integer(topic_id)
+
+    topic =
+      Topic
+      |> Repo.get(topic_id)
+      |> Repo.preload(:comments)
+
+    {:ok, %{comments: topic.comments}, assign(socket, :topic, topic)}
+  end
+end
