f262a102ed13581d2d3ab5e0f6e22028818f8f6e
Andrea Di Giorgi <adigiorgi@outlook.com>
2021-04-14 20:05:27 -0700

    Show comments

--- discuss/assets/js/socket.js
+++ discuss/assets/js/socket.js
@@ -1,14 +1,21 @@
-import { Socket } from "phoenix"
+import { htmlEscape } from "escape-goat";
+import { Socket } from "phoenix";
 
 let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
+const renderComments = (comments) => {
+  const renderedComments = comments.map((comment) => `<li class="collection-item">${htmlEscape(comment.content)}</li>`);
+
+  document.querySelector('.collection').innerHTML = renderedComments.join('');
+};
+
 const createSocket = (topicId) => {
   let channel = socket.channel(`comments:${topicId}`, {})
 
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("ok", resp => renderComments(resp.comments))
     .receive("error", resp => { console.log("Unable to join", resp) })
 
   document.querySelector('button').addEventListener('click', () => {
