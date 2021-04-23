0123fc02a9694cbdf8364e4f96c8deb23dce1387
Andrea Di Giorgi <adigiorgi@outlook.com>
2021-04-22 19:44:56 -0700

    Assign comments to users

--- discuss/lib/discuss_web/channels/comments_channel.ex
+++ discuss/lib/discuss_web/channels/comments_channel.ex
@@ -4,15 +4,16 @@ defmodule DiscussWeb.CommentsChannel do
 
   def handle_in(_name, %{"content" => content}, socket) do
     topic = socket.assigns.topic
+    user_id = socket.assigns.user_id
 
     changeset =
       topic
-      |> Ecto.build_assoc(:comments)
+      |> Ecto.build_assoc(:comments, user_id: user_id)
       |> Comment.changeset(%{content: content})
 
     case Repo.insert(changeset) do
       {:ok, comment} ->
-        broadcast!(socket, "comments:#{socket.assigns.topic.id}:new", %{comment: comment})
+        broadcast!(socket, "comments:#{topic.id}:new", %{comment: comment})
         {:reply, :ok, socket}
 
       {:error, _reason} ->
